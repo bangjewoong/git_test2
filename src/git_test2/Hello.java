2c5c69ea2c145948006a643d46afcae777420d28
Bang, Jewoong <dhkddnd1234@gmail.com>
2025-03-12 10:42:15 +0900

    출력 메세지 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,9 +1,9 @@
 package git_test2;
 
-public class hello {
+public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("안녕하세요");
 
 	}
 
